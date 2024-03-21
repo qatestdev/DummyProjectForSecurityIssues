1c3f6f127547d2fe780a0f41be904ed1d47d2d5f
devcodacytestuser <devcodacytestuser@codacy.com>
2024-03-21 07:41:27 +0000

    Create PR with security issues

--- test.js
+++ test.js
@@ -1,1 +1,17 @@
 console.log('Hello World');
+
+var nums = [];
+
+function createNumberFunctions() { 
+    for (let i = 0; i < 10; i++) {
+        nums[i] = function (j) {
+            return i + j;
+        };
+    }
+
+    return nums;
+}
+
+const numberFunctions = createNumberFunctions();
+console.log(numberFunctions[0](2));
+
